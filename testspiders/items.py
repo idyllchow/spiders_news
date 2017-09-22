48dce02988c7721c544331d49f16ad732982ebac
shibo <zhoushibo@openplay.com>
2017-09-22 16:33:39 +0800

    init repo

--- testspiders/items.py
+++ testspiders/items.py
@@ -1,4 +1,6 @@
 import scrapy
+from scrapy.loader import Identity
+from scrapy.loader import ItemLoader
 
 
 class Page(scrapy.Item):
@@ -8,3 +10,23 @@ class Page(scrapy.Item):
     referer = scrapy.Field()
     newcookies = scrapy.Field()
     body = scrapy.Field()
+
+
+class MynewsItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    title = scrapy.Field()
+    urls = scrapy.Field()
+    link = scrapy.Field()
+    content = scrapy.Field()
+    author = scrapy.Field()
+    date = scrapy.Field()
+    last_updated = scrapy.Field()
+    # 图片
+    image_urls = scrapy.Field()
+    images = scrapy.Field()
+
+
+class NYItemLoader(ItemLoader):
+    default_item_class = MynewsItem
+    default_output_processor = Identity()
