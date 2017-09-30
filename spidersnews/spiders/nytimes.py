ca576719e1ea2a8cb421df81715546a8f2e5d6b2
shibo <zhoushibo@openplay.com>
2017-09-30 18:09:04 +0800

    join en

--- spidersnews/spiders/nytimes.py
+++ spidersnews/spiders/nytimes.py
@@ -38,7 +38,7 @@ class NYSpider(scrapy.Spider):
     def parse_list(self, response):
         urls = response.xpath("//a/@href").extract()
         for url in urls:
-            yield scrapy.Request(url, self.parse_news)
+            yield scrapy.Request(url, self.parse_news_dual)
 
     def parse_news(self, response):
         data = response.xpath("//div[@class='content chinese']")
