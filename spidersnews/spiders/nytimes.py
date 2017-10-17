e895da75166502c5190fad9123a837a10634f7db
shibo <zhoushibo@openplay.com>
2017-10-17 17:14:59 +0800

    dual parse

--- spidersnews/spiders/nytimes.py
+++ spidersnews/spiders/nytimes.py
@@ -88,7 +88,7 @@ class NYSpider(scrapy.Spider):
             for c in content:
                 for ce in content_en:
                     dual = dual + c + '\n' + ce + '\n'
-        item['dual'] = dual
+        item['content_dual'] = dual
 
         if (len(item['content']) != 0):
             yield item
